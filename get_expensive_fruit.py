5f43a8456d48384fafb6c472bbb2284fb6806d58
Farhod Rozikov <fara.rozikov@gmail.com>
2023-01-14 13:39:04 +0500

    fet expensive fruit

--- get_expensive_fruit.py
+++ get_expensive_fruit.py
@@ -8,6 +8,22 @@ def get_expensive_fruit(data:str)->str:
         name of the most expensive fruit
     """
     # your code here
-    return  
+    prices = []
+    names = []
+    rows = data.split('\n')[1:]
+    for row in rows:
+        names.append(row.split(',')[0])
+        prices.append(float(row.split(',')[1]))
+        
+    c = prices[0]
+    for f in prices:
+        if c < f:
+            c = f
+    
+    return names[prices.index(c)]
+
+f = open('fruits.csv', encoding='UTF-8')
+d_file = f.read()
+print(get_expensive_fruit(d_file))
 
 
